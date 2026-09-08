4d11c698f4f6cc1943a4edfcff62218bccaab03f
Kaushalp18 <99526278+Kaushalp18@users.noreply.github.com>
2026-09-08 22:39:43 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> hset = new HashSet<>();
+        for (int n : nums) {
+            if (hset.contains(n)) {
+                return true;
+            }
+            hset.add(n);
+        }
+        return false;
+    }
+}
